73e0c8440eb157037c062077b6bbe25ae2b45ea0
Naresh Bhusal <nares.bhusal123@gmail.com>
2019-05-19 12:50:18 +0545

    Updates

---
+++ precache-manifest.b9292a8e074a5bf7b6a1d85070992a70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b5db6c4dba6f58384fdf2990dafe4c",
+    "url": "/Shows/index.html"
+  },
+  {
+    "revision": "5aaaccc445fe0c564149",
+    "url": "/Shows/static/css/2.c1f55b3e.chunk.css"
+  },
+  {
+    "revision": "31d77969ed6d11bc0ee6",
+    "url": "/Shows/static/css/main.10276440.chunk.css"
+  },
+  {
+    "revision": "5aaaccc445fe0c564149",
+    "url": "/Shows/static/js/2.9e07348b.chunk.js"
+  },
+  {
+    "revision": "31d77969ed6d11bc0ee6",
+    "url": "/Shows/static/js/main.e26eb08a.chunk.js"
+  },
+  {
+    "revision": "37e64b7b423ee7927a06",
+    "url": "/Shows/static/js/runtime~main.11ee6706.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Shows/static/media/SEGA.12a3db8b.woff"
+  }
+]);
